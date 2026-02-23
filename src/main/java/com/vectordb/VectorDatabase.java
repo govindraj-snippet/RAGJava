5d506bbe314144f947f75bd59ae7e5ab2b266dd0
govindraj-snippet <2023bit035@sggs.ac.in>
2026-02-23 11:33:55 +0530

    day3 done

--- src/main/java/com/vectordb/VectorDatabase.java
+++ src/main/java/com/vectordb/VectorDatabase.java
@@ -6,6 +6,19 @@ public class VectorDatabase {
 
     private List<Document>documents ;
 
+    public VectorDatabase(List<Document>documents ){
+        this.documents = documents ; 
+    }
+    public void insert(Document doc ){
+        this.documents.add(doc) ;
+    }
+    public List<Document> getDocuments(){
+        return this.documents ; 
+    }
+    public int getSize(){
+        return this.documents.size() ; 
+    }
+
     
 
 
